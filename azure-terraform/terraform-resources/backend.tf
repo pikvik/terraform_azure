852d871812a92cf80e07f64bcb9941806fae6a8f
Vikas Kumar <vikas.kumar.pandey08@gmail.com>
2020-04-02 23:21:38 +0530

    removed some random files pushed with

--- azure-terraform/terraform-resources/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    storage_account_name = "terraformstoarge"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-    access_key           = "zAJTHzQce3JmO1CCZgCGsHWEqH1m/YfBjWXEGUt8o3DmC+3sXaxQistIOJCQtGYlBr7/yvifA50lSnd3ygepiQ=="
-  }
-}
--- azure-terraform/terraform-resources/variables.tf
+++
@@ -1,1 +0,0 @@
-variable "client_secret" {}
